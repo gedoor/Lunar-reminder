0c0f67ce3ae40df3dfda179188b7b0dcd884de85
GKF <gekunfei@live.com>
2017-03-07 17:20:26 +0800

    Initial commit

---
+++ app/src/main/java/gedoor/kunfei/lunarreminder/ReminderActivity.java
@@ -0,0 +1,19 @@
+package gedoor.kunfei.lunarreminder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GKF on 2017/3/7.
+ */
+
+public class ReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+
+    }
+
+}
